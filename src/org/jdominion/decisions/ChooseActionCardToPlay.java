ivadla/jdominion

commit 4837ea2d3814c29e20cf7501538e101bdfbef130

use method isTerminalAction in the decision ChooseActionCardToPlay

src/org/jdominion/decisions/ChooseActionCardToPlay.java
@@ -3,10 +3,8 @@ package org.jdominion.decisions;
 import java.util.List;
 
 import org.jdominion.Card;
-import org.jdominion.Hand;
 import org.jdominion.Card.Type;
-import org.jdominion.effects.AddActions;
-import org.jdominion.effects.CardEffect;
+import org.jdominion.Hand;
 
 public class ChooseActionCardToPlay extends ChooseCardsFromHand {
 
@@ -31,10 +29,8 @@ public class ChooseActionCardToPlay extends ChooseCardsFromHand {
 	@Override
 	protected Card chooseCardForDefaultAnswer(Hand hand) {
 		for (Card card : hand.getCardList()) {
-			for (CardEffect effect : card.getEffects()) {
-				if (effect instanceof AddActions) {
-					return card;
-				}
+			if (!card.isTerminalAction()) {
+				return card;
 			}
 		}
 		for (Card card : hand.getCardList()) {

commit 9c8afa90578eef2f6320ec64a59beb24b6ff080c

fix a NullPointerException when the ai chooses an action card.
This Bug has been introduced in commit 4837ea2d3814c29e20cf7501538e101bdfbef130

src/org/jdominion/decisions/ChooseActionCardToPlay.java
@@ -29,7 +29,7 @@ public class ChooseActionCardToPlay extends ChooseCardsFromHand {
 	@Override
 	protected Card chooseCardForDefaultAnswer(Hand hand) {
 		for (Card card : hand.getCardList()) {
-			if (!card.isTerminalAction()) {
+			if (card.isOfType(Type.ACTION) && !card.isTerminalAction()) {
 				return card;
 			}
 		}