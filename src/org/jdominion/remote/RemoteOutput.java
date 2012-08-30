5d0c52a1fd961a1e0d923e71dacd9c442fb7e9e2
ivadla <ivadla@arcor.de>
2012-08-30 20:12:28 +0200

    update the gui after a card was played

--- src/org/jdominion/remote/RemoteOutput.java
+++ src/org/jdominion/remote/RemoteOutput.java
@@ -3,18 +3,19 @@ package org.jdominion.remote;
 import java.rmi.RemoteException;
 
 import org.jdominion.event.CardBought;
-import org.jdominion.event.CardGained;
-import org.jdominion.event.CardPlayed;
 import org.jdominion.event.CardDiscarded;
+import org.jdominion.event.CardGained;
+import org.jdominion.event.CardPlayFinished;
+import org.jdominion.event.CardPlayed;
+import org.jdominion.event.CardTrashed;
 import org.jdominion.event.CardsDrawn;
 import org.jdominion.event.CardsRevealed;
 import org.jdominion.event.CardsSetAside;
-import org.jdominion.event.CardTrashed;
 import org.jdominion.event.Event;
 import org.jdominion.event.EventManager;
+import org.jdominion.event.EventManager.Duration;
 import org.jdominion.event.GameEnded;
 import org.jdominion.event.IEventHandler;
-import org.jdominion.event.EventManager.Duration;
 
 public class RemoteOutput implements IEventHandler {
 
@@ -39,6 +40,7 @@ public class RemoteOutput implements IEventHandler {
 		eventManager.addEventHandler(this, CardsDrawn.class, Duration.FOREVER);
 		eventManager.addEventHandler(this, CardGained.class, Duration.FOREVER);
 		eventManager.addEventHandler(this, CardPlayed.class, Duration.FOREVER);
+		eventManager.addEventHandler(this, CardPlayFinished.class, Duration.FOREVER);
 		eventManager.addEventHandler(this, CardTrashed.class, Duration.FOREVER);
 		eventManager.addEventHandler(this, CardsRevealed.class, Duration.FOREVER);
 		eventManager.addEventHandler(this, CardsSetAside.class, Duration.FOREVER);
