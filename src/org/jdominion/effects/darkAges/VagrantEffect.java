e96ee8ca18a1e438a71ac85133af23c8b200f0aa
ivadla <ivadla@arcor.de>
2012-08-31 20:12:42 +0200

    fix vagrant if there is no card to reveal

--- src/org/jdominion/effects/darkAges/VagrantEffect.java
+++ src/org/jdominion/effects/darkAges/VagrantEffect.java
@@ -12,6 +12,9 @@ public class VagrantEffect extends CardEffectAction {
 	@Override
 	public boolean execute(Player activePlayer, Turn currentTurn, Supply supply) {
 		Card revealedCard = activePlayer.revealCard();
+		if (revealedCard == null) {
+			return false;
+		}
 		if (revealedCard.isOfType(Type.CURSE) || revealedCard.isOfType(Type.RUINS) || revealedCard.isOfType(Type.SHELTER)
 				|| revealedCard.isOfType(Type.VICTORY)) {
 			activePlayer.getHand().add(revealedCard);
